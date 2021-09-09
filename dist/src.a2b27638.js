2419896d151ff5cdc125dfc03ad675f492c34dcb
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-09-09 08:50:02 -0400

    Crear formulario para ingresar todo

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -118,96 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/App.js":[function(require,module,exports) {
-var form = document.querySelector("#register");
-var cant = document.querySelector("#cant-items");
-var price = document.querySelector("#precio-items");
-var state = document.querySelector("#codigo-estado");
-var description = document.querySelector("#descripcion-items");
-/*UT               	6.65%
-NV               	8.00%
-TX                	6.25%
-AL                	4.00%
-CA                	8.25%*/
-
-function calculateTot(a, b) {
-  return a * b;
-} //var cod = document.getElementById("producto").value;
-
-
-function calculateSubTot(priceTot, state) {
-  var tax;
-
-  switch (state) {
-    case "UT":
-      tax = 0.0665;
-      break;
-
-    case "NV":
-      tax = 0.08;
-      break;
-
-    case "TX":
-      tax = 0.0625;
-      break;
-
-    case "AL":
-      tax = 0.04;
-      break;
-
-    case "CA":
-      tax = 0.0825;
-      break;
-
-    default:
-      break;
-  }
-
-  return priceTot + priceTot * tax;
-}
-/*
-            1000            	3%
-        	3000            	5%
-        	7000	        	7%
-        	10000          	    10%
-        	30000          	    15%
-*/
-
-
-function calculateDiscount(total) {
-  var discount = 0;
-
-  if (total >= 30000) {
-    discount = 0.15;
-  } else if (total >= 10000) {
-    discount = 0.10;
-  } else if (total >= 7000) {
-    discount = 0.07;
-  } else if (total >= 3000) {
-    diacaunt = 0.05;
-  } else if (total >= 1000) {
-    discount = 0.03;
-  }
-
-  return total * discount;
-}
-
-function totalGlobal(totTax, discaunt) {
-  return totTax - discaunt;
-}
-
-form.addEventListener("submit", function (event) {
-  var msg = "La cantidad registrada de items es: ";
-  var msgPrice = "El precio del producto es: ";
-  var msgTotalprice = "El precio total del pedido sin impuestos ni descuentos es: ";
-  var msgTax = "El precio Subtotal con impuesto es: ";
-  var msgDiscaunt = "El descuento por su compra es : ";
-  var msgTotGlobal = "El precio total global es: ";
-  var msgDescription = "Descripcion del producto: ";
-  var tot = calculateTot(cant.value, price.value);
-  var subTot = calculateSubTot(tot, state.value);
-  var discountTot = calculateDiscount(subTot);
-  alert(msg + cant.value + "\n" + msgPrice + price.value + "\n" + msgTotalprice + tot + "\n" + msgTax + subTot + "\n" + msgDiscaunt + discountTot + "\n" + msgTotGlobal + totalGlobal(subTot, discountTot) + "\n" + msgDescription + description.value);
-});
+alert("Hola desde javascript");
 },{}],"src/index.js":[function(require,module,exports) {
 "use strict";
 
@@ -240,7 +151,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53234" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54985" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
